334e99050f8d3f0405b9d6d7a78207ef2f67384a
yanko <yanko.qiu@gmail.com>
2024-12-07 16:42:59 +0800

    chore: 项目结构

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,7 @@ import uni from "@dcloudio/vite-plugin-uni";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [uni()],
-
-    build: {
-        lib: {
-            entry: "./src/uni_modules/net-finch/js_sdk/index.ts",
-            name: "net-finch",
-            fileName: "net-finch"
-        },
-        rollupOptions: {
-            external: ["vue"],
-            output: {
-
-            }
-        }
+    server: {
+        port: 3000,
     }
 });
